4e4d57f3c44bdfc388c44039965638e37b35bfb2
David <davidtn52@gmail.com>
2019-10-02 00:18:56 +0200

    add seed

--- database/seeds/ProductsTableSeeder.php
+++ database/seeds/ProductsTableSeeder.php
@@ -19,6 +19,17 @@ class ProductsTableSeeder extends Seeder
         // factory(Product::class,100)->create();
         // factory(ProductImage::class,200)->create();
         // Model Poblacion atraves de Relaciones
+        
+        $categories = factory(Category::class,5)->create();
+
+        $categories->each(function($category) {
+            $products = factory(Product::class,20)->make();
+            $category->products()->saveMany($products);
+
+            $products->each(function($product) {
+                $images = factory(ProductImage::class,5)->make();
+                $product->images()->saveMany($images);
+            });
 
         
     }
