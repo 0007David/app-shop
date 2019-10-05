c9a9f47d710853e00aedb51f6c2ab51210ae8a05
David <davidtn52@gmail.com>
2019-10-05 11:10:36 +0200

    com movil api

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,7 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/test','Api\LoginController@test');
+Route::post('/login','Api\LoginController@login');
+
